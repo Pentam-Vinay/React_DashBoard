1c4cc05ff4e5a534064180c49925aa93496818b7
vinay <vinaypentam@gmail.com>
2024-09-27 13:57:02 +0530

    Change

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
 //export const API_URL="http://localhost:8090"
 
-export const API_URL="https://backend-node-jsvthree.onrender.com"
+export const API_URL="https://backend-nodejs-vthree.onrender.com"
 
